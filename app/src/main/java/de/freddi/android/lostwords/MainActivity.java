4d1ed27583879cadf553cf550ca217e35279bcad
Freddi <muff99@outlook.com>
2016-03-28 13:22:50 +0200

    Favorites Dialog anderes Design mit Linientrennern

--- app/src/main/java/de/freddi/android/lostwords/MainActivity.java
+++ app/src/main/java/de/freddi/android/lostwords/MainActivity.java
@@ -1,5 +1,7 @@
 package de.freddi.android.lostwords;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
